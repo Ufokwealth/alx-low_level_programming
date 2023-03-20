a570db3ec47c84bc360e4010ba0420c3115d9d82
ufokwealth <ufokeffiom@gmail.com>
2023-03-20 07:58:29 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,11 +7,11 @@
 */
 int main(void)
 {int k;
-for (k = 0; k < 100; k++)
+for (k = 0; k < 10; k++)
 {
 putchar((k / 10) + '0');
 putchar((k % 10) + '0');
-if (k != 99)
+if (k != 9)
 {
 putchar(',');
 putchar(' ');
