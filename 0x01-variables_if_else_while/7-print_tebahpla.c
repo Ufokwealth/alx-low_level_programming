20c2d647e427f2f37f7faecfc372b78f41257db7
ufokwealth <ufokeffiom@gmail.com>
2023-03-19 17:25:20 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (Succes)
+*/
+int main(void)
+{
+char d;
+for (d = 'z'; d <= 'a'; d--)
+putchar(d);
+putchar('\n');
+return (0);
+}
