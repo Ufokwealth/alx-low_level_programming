347e27bc4ce175b2f8569dbe6f52e5c9212ff937
ufokwealth <ufokeffiom@gmail.com>
2023-03-19 17:06:47 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+* main - Entry point
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+char k;
+for (k = '0'; k <= '9'; k++)
+putchar(k);
+putchar('\n');
+return (0);
+}
