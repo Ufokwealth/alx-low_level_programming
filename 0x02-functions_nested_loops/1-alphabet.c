d6cfe2c1e7084356adcdb0f9c9251131b661c289
ufokwealth <ufokeffiom@gmail.com>
2023-03-22 04:58:49 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_alphabet - prints the alphabet in lowercase.
+* Return: Always 0 (Success)
+*/
+void print_alphabet(void)
+{
+int i;
+for (i = 'a''; i <= 'z'; i++)
+_putchar(i);
+_putchar('\n');
+}
