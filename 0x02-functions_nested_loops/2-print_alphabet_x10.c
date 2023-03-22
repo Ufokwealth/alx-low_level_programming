5cad77c0594d9b65903261609551b706b201a2ff
ufokwealth <ufokeffiom@gmail.com>
2023-03-22 15:11:24 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet - prints alphabet 10 times
+* Return: Always 0 (Success)
+*/
+void print_alphabet_x10(void)
+{
+char i;
+int k;
+for (k = 0; k < 10; k++)
+{
+for (i = 'a'; i <= 'z'; i++)
+{
+_putchar(i);
+}
+_putchar('\n');
+}
+}
