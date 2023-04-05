7ff18174a1a99dca57db443b8baebe3d31e11f8f
ufokwealth <ufokeffiom@gmail.com>
2023-04-05 14:39:56 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,11 +6,12 @@
 */
 void _puts_recursion(char *s)
 {
-if (*s)
+if (*s == '\0')
 {
-putchar(*s);
-_puts_recursion(s + 1);
+putchar('\n');
+return;
 }
-else
-_putchar('\n');
+putchar(*s);
+s++;
+_puts_recursion(s);
 }
