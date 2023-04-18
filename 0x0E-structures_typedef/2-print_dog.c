16f884a20f0a1ed5571ccf01dfe1e7fd597cd283
ufokwealth <ufokeffiom@gmail.com>
2023-04-18 15:43:11 -0700

    2-print_dog.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdio.h>
+/**
+ * print_dog -  a function that prints a struct dog
+ * @d: struct to initialize
+ * Return: 0
+ */
+void print_dog(struct dog *d)
+{
+if (d == NULL)
+return;
+if (d->owner == NULL)
+d->owner = "(nil)";
+printf("Name: %s\nAge: %0.6f\nOwner: %s\n", d->name, d->age, d->owner);
+}
