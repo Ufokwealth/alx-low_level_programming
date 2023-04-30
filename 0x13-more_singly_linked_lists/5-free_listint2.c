e5bc11ae57ede72d9698634717407bf76379e6e8
Ufokwealth <ufokeffiom@gmail.com>
2023-04-30 16:21:09 -0700

    5-free_listint2.c

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that frees a list
+ * @head: pointer to linstint_t **head)
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *temp;
+if (head == NULL)
+return;
+temp = *head;
+while (*head)
+{
+temp = *head;
+(*head) = (*head)->next;
+free(temp);
+}
+*head = NULL;
+}
