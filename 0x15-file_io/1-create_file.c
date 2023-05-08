38764e1d5b352dd39961b6f6f0ba0200a5cd26f7
Ufokwealth <ufokeffiom@gmail.com>
2023-05-08 15:18:33 -0700

    created strlen

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * _strlen - compute the length of NULL-terminated string
+ * @str: the string to measure
+ * Return: the length of str, or -1 if str is NULL
+ */
+ssize_t _strlen(const char *str)
+{
+ssize_t len = 0;
+if (!str)
+return (-1);
+while (*str++)
+++len;
+return (len);
+}
+/**
  * create_file - function that creates a file
  * @filename: name of file
  * @text_content: content file
@@ -9,16 +23,17 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int a = 0, file;
-if (filename == NULL)
+ssize_t f_written = 0;
+int a;
+if (!filename)
 return (-1);
-if (text_content[a] != '\0')
-{
-a++;
-}
-file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (file == -1)
+a = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+if (a < 0)
 return (-1);
-write(file, text_content, a);
+if (text_content)
+f_written = write(a, text_content, _strlen(text_content));
+close(a);
+if (f_written < 0)
+return (-1);
 return (1);
 }
